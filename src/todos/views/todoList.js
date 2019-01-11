fc6e9ae90a65605ba9cb46c3cc438b5a5979b949
rosalind.zhang <rosalind.zhang@yatsenglobal.com>
2019-01-11 17:15:31 +0800

    transitionGroup动画

--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -1,52 +1,31 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
+import TransitionGroup from 'react-addons-css-transition-group'
 
-import {toggleTodo, removeTodo} from '../actions'
-import {FilterTypes} from '../../constants'
 import TodoItem from './todoItem'
-
 import {selectVisibleTodos} from './reselect'
 
+import './todoItem.css'
+
 class TodoList extends Component {
-  constructor(props, context) {
-    super(...arguments)
-  }
   render() {
     return (
       <ul className="todo-list">
+        <TransitionGroup transitionName="fade" 
+          transitionEnterTimeout={500} 
+          transitionLeaveTimeout={200}>
         {
           this.props.todos.map(item => (
             <TodoItem key={item.id} item={item}/>
-          //   <li className="todo-item" key={item.id}> 
-          //     <span>{item.id}</span>
-          //     <input className="toggle" type="checkbox" readOnly checked = {item.completed}
-          //       onClick={() => this.props.onToggleTodo(item.id)} />
-          //     <label className="text">{item.text}</label>
-          //     <button className="remove" onClick={()=>this.props.onRemoveTodo(item.id)}>×</button>
-          // </li>
           ))
         }
+        </TransitionGroup>
       </ul>
     )
   }
 }
 
-// const selectvisibleTodos = (todos, filter) => {
-//   switch(filter) {
-//     case FilterTypes.ALL: {
-//       return todos
-//     }
-//     case FilterTypes.COMPLETED: {
-//       return todos.filter(item => item.completed)
-//     }
-//     case FilterTypes.UNCOMPLETED: {
-//       return todos.filter(item => !item.completed)
-//     }
-//     default:
-//       return todos
-//   }
-// }
 const mapStateToProps = (state) => {
   return {
     todos: selectVisibleTodos(state)
